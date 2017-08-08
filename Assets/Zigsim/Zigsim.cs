2784999b3c17882a8d3cb167f68a037b0086ca6d
MHD Yamen Saraiji <mrayyamen@gmail.com>
2017-08-08 16:54:08 +0900

    initial commit

---
+++ Assets/Zigsim/Zigsim.cs
@@ -0,0 +1,135 @@
+﻿//Simple wrapper for Zig-Sim
+//Written by MHD Yamen Saraiji - yamen@myamens.com
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+public delegate void PacketReceivedHandler(object sender, string PacketData);
+
+public class Zigsim : MonoBehaviour {
+	UdpClient client;
+	public int Port  = 50000;
+	private IPEndPoint mRemoteIpEndPoint;
+	private Socket     mListener;
+	private byte[]     mReceiveBuffer;
+	private string     mPacket;
+	private const int  kMaxSubPacketSize       = 1400;
+
+	bool isDone=false;
+	Thread _udpGrabber;
+
+	public event PacketReceivedHandler PacketNotification;
+
+	// Use this for initialization
+	void Start () {
+		mReceiveBuffer = new byte[kMaxSubPacketSize];
+		mPacket        = System.String.Empty;
+
+		mRemoteIpEndPoint = new IPEndPoint(IPAddress.Any, Port);
+		mListener = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		mListener.Bind(mRemoteIpEndPoint);
+
+		mListener.Blocking          = false;
+		mListener.ReceiveBufferSize = 128*1024;
+
+		_udpGrabber = new Thread (new ThreadStart (UDPUpdate));
+		_udpGrabber.Start ();
+
+	}
+
+	void OnDestroy()
+	{
+		isDone=true;
+		mListener.Close ();
+		_udpGrabber.Join ();
+	}
+
+	void Update()
+	{
+		if(data.sensordata!=null)
+		transform.rotation = data.sensordata.quaternion;
+	}
+
+	void UDPUpdate()
+	{
+		while (!isDone) {
+			
+			UDPRead ();
+		}
+
+	}
+	public void UDPRead()
+	{
+		try
+		{
+			int bytesReceived = mListener.Receive(mReceiveBuffer);
+
+			mPacket = Encoding.ASCII.GetString(mReceiveBuffer, 0, bytesReceived);
+
+
+			//== ok packet has been created from sub packets and is complete ==--
+			Parse(mPacket);
+			//== notify listeners ==--
+			if(PacketNotification!=null)
+				PacketNotification(this, mPacket);
+
+		}
+		catch(System.Exception ex)
+		{}
+	}
+
+
+	public ZigJSON data=new ZigJSON();
+
+	public bool Parse(string message)
+	{
+		JsonUtility.FromJsonOverwrite(message,data);
+
+		//cheap way..
+		if (message.Contains ("accel")) {
+			data.sensordata.hasAccel = true;
+		}else data.sensordata.hasAccel = false;
+
+		if (message.Contains ("gyro")) {
+			data.sensordata.hasGyro = true;
+		}else data.sensordata.hasGyro = false;
+
+		if (message.Contains ("gps")) {
+			data.sensordata.hasGPS = true;
+		}else data.sensordata.hasGPS = false;
+
+		if (message.Contains ("quaternion")) {
+			data.sensordata.hasQuaternion = true;
+		}else data.sensordata.hasQuaternion = false;
+
+		if (message.Contains ("compass")) {
+			data.sensordata.hasCompass = true;
+		}else data.sensordata.hasCompass = false;
+
+		if (message.Contains ("gravity")) {
+			data.sensordata.hasGravity = true;
+		}else data.sensordata.hasGravity = false;
+
+		if (message.Contains ("touch")) {
+			data.sensordata.hasTouch = true;
+		}else data.sensordata.hasTouch = false;
+
+		if (message.Contains ("pressure")) {
+			data.sensordata.hasPressure = true;
+		}else data.sensordata.hasPressure = false;
+
+		if (message.Contains ("miclevel")) {
+			data.sensordata.hasMiclevel = true;
+		}else data.sensordata.hasMiclevel = false;
+
+		return true;
+	}
+
+}
---
+++ Assets/Zigsim/ZigsimJSON.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Device{
+	public string name="unkown";
+	public string uuid="unkown";
+};
+
+[Serializable]
+public class Time{
+	public int year;
+	public int month;
+	public int day;
+	public int hour;
+	public int minuite;
+	public float second;
+	public string timeString="";
+};
+
+[Serializable]
+public class Pressure{
+	public float pressure;
+	public float altitude;
+};
+
+[Serializable]
+public class Compass{
+	public bool faceup;
+	public float angle;
+};
+
+[Serializable]
+public class GPS{
+	public float latitude;
+	public float longitude;
+};
+
+[Serializable]
+public class TouchPoint{
+	public float x;
+	public float y;
+	public float radius;
+	public float force;
+};
+
+[Serializable]
+public class Beacon{
+	public string uuid="unkown";
+	public int rssi;
+	public int major;
+	public int minor;
+};
+
+
+[Serializable]
+public class MicLevel{
+	public float max;
+	public float average;
+}
+
+[Serializable]
+public class SensorData
+{
+
+	public bool hasAccel;
+	public Vector3 accel;
+
+	public bool hasGyro;
+	public Vector3 gyro;
+
+	public bool hasGravity;
+	public Vector3 gravity;
+
+	public bool hasQuaternion;
+	public Quaternion quaternion;
+
+	public bool hasCompass;
+	public Compass compass;
+
+	public bool hasPressure;
+	public Pressure pressure;
+
+	public bool hasGPS;
+	public GPS gps;
+
+	public bool hasTouch;
+	public bool hasTouch2D;
+	public bool hasTouchRadius;
+	public bool hasTouchForce;
+	public TouchPoint[] touch;
+
+	public bool hasMiclevel;
+	public MicLevel miclevel;
+
+	public bool hasBeacon;
+	public Beacon[] beacons;
+}
+
+[Serializable]
+public class ZigJSON{
+	
+	public Device device;
+	public Time time;
+
+	public SensorData sensordata;
+	public ZigJSON()
+	{
+	}
+
+
+};
